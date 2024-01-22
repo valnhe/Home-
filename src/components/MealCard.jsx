c2896f352c681019e80ab458bf556062f866b46c
valnhe <vpazcastle@gmail.com>
2024-01-22 18:40:29 -0300

    Update MealCard image size and styling

--- src/components/MealCard.jsx
+++ src/components/MealCard.jsx
@@ -2,7 +2,7 @@ function MealCard({img, title, url}) {
     return (
         <li className="bg-[#394050] px-[8px] pt-[8px] rounded-lg">
             <a href={`/meals/${url}`}>
-                <img src={img} alt={title} className="rounded-md"/>
+                <img src={img} alt={title} className="rounded-md h-[180px] w-full object-cover object-center"/>
                 <h3 className="my-[12px]">{title}</h3>
             </a>
         </li>
